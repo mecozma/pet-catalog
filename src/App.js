8147d4a82f84bbd48b31e98e893ce8eafd3fecef
mecozma <mecozma@gmail.com>
2019-06-11 21:57:56 +0100

    Added the Pet component and installed Tabletop.js to convert the Google spreadsheet to JSON

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import Pet from './Pet/Pet';
 import "./App.css";
 
+import Tabletop from 'tabletop';
+
 class App extends Component {
+
+  componentDidMount() {
+    Tabletop.init({
+      key: "1IS_wjEiG_nUrnoOa49wOijx2Fgg5inYHYzpZePPnfn0",
+      callback: googleData => {
+       console.log(googleData);
+      },
+      simpleSheet: true
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Pet Catalog Test</h1>
+        <Pet name="Rex" />
       </div>
     );
   }
---
+++ src/Pet/Pet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pet = (props) => {
+  return(
+    <div>
+      {props.name}
+    </div>
+  )
+}
+
+export default Pet;
