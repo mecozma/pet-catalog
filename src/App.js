a64a86bd03e2fd9bfd769feb099d74aeb8958c13
mecozma <mecozma@gmail.com>
2019-06-12 11:50:21 +0100

    Added state to App.js component to manage the data retrieved from the database and displayed a list of Pets.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from "react";
-import Pet from './Pet/Pet';
+import Pet from "./Pet/Pet";
 import "./App.css";
 
-import Tabletop from 'tabletop';
+import Tabletop from "tabletop";
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      pets: []
+    };
+  }
+/* ComponentDidMount() is invoked immediately after a component is mounted. 
+Initialization that requires DOM nodes should go here. If you need to load
+ data from a remote endpoint, this is a good place to instantiate the network
+  request. Setting state in this method will trigger a re-rendering. */
   componentDidMount() {
     Tabletop.init({
       key: "1IS_wjEiG_nUrnoOa49wOijx2Fgg5inYHYzpZePPnfn0",
       callback: googleData => {
-       console.log(googleData);
+        this.setState({
+          pets: googleData,
+          isLoading: false
+        });
+        console.log(this.state.pets);
       },
       simpleSheet: true
     });
   }
   render() {
+    const { isLoading, pets } = this.state;
     return (
       <div className="App">
         <h1>Pet Catalog Test</h1>
-        <Pet name="Rex" />
+        {/* If isLoading property is false and the array pets is no empty anymore,
+         the .map method will iterate through the array and will return a list of 
+         all the pets in the array*/}
+        {!isLoading && pets.length > 0
+          ? pets.map((pet, i) => {
+              return (
+                <Pet
+                  key={pet.Animal_ID + i}
+                  name={pet.Animal_Name}
+                  petType={pet.animal_type}
+                  petBreed={pet.Animal_Breed}
+                  petLocation={pet.Address}
+                />
+              );
+            })
+          : null}
+        {
+          isLoading ? <div>Loading...</div> : null
+        }
       </div>
     );
   }
--- src/Pet/Pet.js
+++ src/Pet/Pet.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Pet.css';
 
 const Pet = (props) => {
   return(
-    <div>
-      {props.name}
+    <div className="Pet">
+      <h3>Pet type: {props.petType}. Breed: {props.petBreed}</h3>
+      <h4>Name:  {props.name}</h4>
+      <h5>Pet location: {props.petLocation}</h5>
     </div>
   )
 }
