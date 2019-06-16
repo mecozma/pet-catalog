00752f95a2f69a8feb9762b21c1933146c3bdfec
mecozma <mecozma@gmail.com>
2019-06-16 13:58:57 +0100

    Updates

---
+++ precache-manifest.d6877fd116150f28b696cf671dd28a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91dba395c56b69e917ab167b1c33c88",
+    "url": "/pet-catalog/index.html"
+  },
+  {
+    "revision": "7862d97743fa039f8bd8",
+    "url": "/pet-catalog/static/css/main.f91634f2.chunk.css"
+  },
+  {
+    "revision": "824653ad0c022802b928",
+    "url": "/pet-catalog/static/js/2.ffa40dd3.chunk.js"
+  },
+  {
+    "revision": "7862d97743fa039f8bd8",
+    "url": "/pet-catalog/static/js/main.a1a15ae4.chunk.js"
+  },
+  {
+    "revision": "a11fc30e3f9c0fd1e7c1",
+    "url": "/pet-catalog/static/js/runtime~main.a3017f40.js"
+  }
+]);
